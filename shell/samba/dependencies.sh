20308f03e5bc53b53c8da89c84c3ec93d2d7b5f4
Ismail MTAALLAH <ismailakira@gmail.com>
2014-10-06 15:14:08 +0100

    add script shell to install dependencies for samba 4 under debian 7

--- shell/samba/dependencies.sh
+++ shell/samba/dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #install under debian 7
-
- apt-get install build-essential python-dev pkg-config libcups2-dev cups krb5-user docbook-xsl libldap2-dev libattr1-dev libacl1-dev attr python-dnspython libreadline-dev li
+sudo apt-get update && sudo apt-get upgrade
+echo "your system is up to date "
+sudo apt-get install -y build-essential python-dev pkg-config libcups2-dev cups krb5-user docbook-xsl libldap2-dev libattr1-dev libacl1-dev attr python-dnspython libreadline-dev libbsd-dev libblkid-dev libpopt-dev cups git acl gdb xsltproc libsasl2-dev libaio-dev libpam-dev valgrind resolvconf autoconf ldap-utils ntp isc-dhcp-server
+echo "Installation of dependencies finished"
  
