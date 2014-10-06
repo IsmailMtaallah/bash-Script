7892be0e32cecbfdca0deef16d0741dab4361847
Ismail MTAALLAH <ismailakira@gmail.com>
2014-10-06 13:57:04 +0100

    correction of directory

---
+++ shell/samba/dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#install under debian 7
+
+ apt-get install build-essential python-dev pkg-config libcups2-dev cups krb5-user docbook-xsl libldap2-dev libattr1-dev libacl1-dev attr python-dnspython libreadline-dev li
+ 
