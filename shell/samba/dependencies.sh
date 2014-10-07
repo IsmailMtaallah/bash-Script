e68f6605e2371f3e97f872b59a09d30c8c84e9f9
Ismail MTAALLAH <ismailakira@gmail.com>
2014-10-07 15:12:46 +0100

    finish dependencies.sh for samba folder

--- shell/samba/dependencies.sh
+++ shell/samba/dependencies.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #install under debian 7
-sudo apt-get update && sudo apt-get upgrade
-echo "your system is up to date "
-sudo apt-get install -y build-essential python-dev pkg-config libcups2-dev cups krb5-user docbook-xsl libldap2-dev libattr1-dev libacl1-dev attr python-dnspython libreadline-dev libbsd-dev libblkid-dev libpopt-dev cups git acl gdb xsltproc libsasl2-dev libaio-dev libpam-dev valgrind resolvconf autoconf ldap-utils ntp isc-dhcp-server
-echo "Installation of dependencies finished"
- 
+
+#update system
+if sudo apt-get update && sudo apt-get upgrade; then
+	echo "your system is now up to date"
+else 
+	echo "I can not update your system, thank you to verify your connection"
+fi
+
+#install dependencies 
+if sudo apt-get install -y build-essential python-dev pkg-config libcups2-dev cups krb5-user docbook-xsl libldap2-dev libattr1-dev libacl1-dev attr python-dnspython libreadline-dev libbsd-dev libblkid-dev libpopt-dev cups git acl gdb xsltproc libsasl2-dev libaio-dev libpam-dev valgrind resolvconf autoconf ldap-utils ntp isc-dhcp-server ; then
+	echo "Installation of dependencies finished"
+else 
+	echo "I can not install all dependencies"
+fi
+
