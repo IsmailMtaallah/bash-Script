bce5a776ca1aa0b214605f264b358437f3d571ce
Ismail MTAALLAH <ismailakira@gmail.com>
2014-10-07 14:02:04 +0100

    improvement of code for sudo.sh

--- shell/OS/sudo.sh
+++ shell/OS/sudo.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-apt-get install sudo
-echo "installation of sudo ended"
+:'
+
+Do not forget to execute this script by using root privilege
+This script designed to install and config sudo under debian 7
+
+'
+
+#Installation of sudo 
+if apt-get install sudo; then 
+    echo "installation of sudo ended"
+else
+    echo "failed"
+fi
+
+#Configuration of /etc/sudoers
 echo "Can you give me your login ? please"
 read login
+#make backup for file /etc/sudoers
+
 cp /etc/sudoers /etc/sudoers.bak
+
+#change permission for applying change
 chmod 660 /etc/sudoers
-echo -e "$login \t ALL=(ALL) \t ALL" >> /etc/sudoers
+
+if echo -e "$login \t ALL=(ALL) \t ALL" >> /etc/sudoers ; then
+    echo "ok"
+else
+    echo "I can not add modification to /etc/sudoers file"
+fi
+
+#restore permission
 chmod 440 /etc/sudoers
 echo "config of sudo ended"
 exit
